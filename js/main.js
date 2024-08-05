c0b65575cdbd89406102a725d5ef32dedab9b40a
omarsanchez <omarcore@gmail.com>
2024-08-05 16:58:31 -0600

    Animaciones listas

--- js/main.js
+++ js/main.js
@@ -27,3 +27,43 @@ const swiper = new Swiper('.swiper', {
         }
     }
 })
+
+// animations
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 300,
+});
+
+sr.reveal('.hero__text',{origin: 'top'})
+
+sr.reveal('.steps__step',{distance: '100px', interval: 100})
+
+sr.reveal('.about__text',{origin: 'left'})
+sr.reveal('.about__img',{origin: 'right', delay:800})
+
+sr.reveal('.testimonial__bg',{delay:800})
+sr.reveal('.testimonial__slider',{delay:1200})
+
+sr.reveal('.brands__img',{delay:600, distance: '100px', interval:100})
+
+sr.reveal('.work__title')
+sr.reveal('.work__subtitle')
+sr.reveal('.work__grid',{delay: 1000})
+
+sr.reveal('.stats')
+sr.reveal('.stats__item',{delay:600, distance: '100px', interval:100})
+
+
+sr.reveal('.news__title')
+sr.reveal('.news__item',{delay:1000, distance: '100px', interval:100})
+
+sr.reveal('.contact__container')
+
+sr.reveal('.footer__item')
+
+
+
+
